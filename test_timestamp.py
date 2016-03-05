c201b4ce384f3f1c448c609de259c68b6478748c
qtfkwk <qtfkwk+kron@gmail.com>
2016-03-04 21:07:15 -0500

    timezone search complete name

--- test_timestamp.py
+++ test_timestamp.py
@@ -1,8 +1,13 @@
 import unittest
 import timestamp
 import tzlocal
+import pytz
 class Test(unittest.TestCase):
     def test_timezone_search_default(self):
         h = timestamp.timezone.search()
         w = tzlocal.get_localzone().zone
         self.assertEqual(h, w)
+    def test_timezone_search_complete_name(self):
+        for w in pytz.all_timezones:
+            h = timestamp.timezone.search(w)
+            self.assertEqual(h, w)
--- timestamp.py
+++ timestamp.py
@@ -1,5 +1,9 @@
 import tzlocal
+import pytz
 class timezone(object):
     @classmethod
-    def search(cls):
-        return tzlocal.get_localzone().zone
+    def search(cls, name=None):
+        if name == None:
+            return tzlocal.get_localzone().zone
+        if name in pytz.all_timezones:
+            return name
