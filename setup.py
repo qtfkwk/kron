20bec7b533e968bb2a87d9e30ecdb369e94b7cab
qtfkwk <qtfkwk+kron@gmail.com>
2016-03-05 17:44:32 -0500

    prep setup

--- setup.py
+++ setup.py
@@ -2,4 +2,14 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         test_suite='test_timestamp',
+        name='timestamp',
+        url='https://github.com/qtfkwk/timestamp',
+        author='qtfkwk',
+        author_email='qtfkwk+timestamp@gmail.com',
+        version='1.0.0',
+        install_requires=[
+            'pytz',
+            'tzlocal',
+        ],
+        py_modules=['timestamp'],
     )
