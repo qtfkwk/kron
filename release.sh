04b8b5a83db1b6d46bd73a3b350d979b0fa5c213
qtfkwk <qtfkwk+kron@gmail.com>
2016-03-13 23:26:50 -0400

    add release script

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -eo pipefail
+
+prev=1.4.1
+new=1.4.2
+
+newre=$(echo $new |sed 's/\./\\./g')
+prevre=$(echo $prev |sed 's/\./\\./g')
+
+run() {
+    echo :: $1
+    $1
+    echo
+}
+
+echo :: grep \\\|\\ $newre README.rst
+grep \|\ $newre README.rst
+echo
+
+run "py.test"
+
+run "sed -i '' s/$prevre/$new/ doc/source/conf.py kron.py setup.py test_kron.py"
+
+run "git clean -dxf"
+
+run "git add *"
+
+echo :: git commit -m release\\ $new
+git commit -m release\ $new
+echo
+
+run "mkdir doc/source/_static"
+run "python setup.py build_sphinx"
+
+run "python setup.py sdist bdist_wheel"
+
+run "git push"
+run "python setup.py upload_sphinx"
+run "python setup.py upload"
+
